98348adee28a2154a54f90820eca0f817362c8b6
Himanshu Gupta <himanshugupta543210@gmail.com>
2024-02-18 15:47:27 +0530

    i

--- index.js
+++ index.js
@@ -3,9 +3,22 @@ const app = express();
 const PORT = 9900;
 
 //ROUTES
-app.get("/", (req, res)=>{
+app.get("/users", (req, res)=>{
     res.end("HEllo! This is home-page.");
 })
+app.get("/users/:id", (req, req)=>{
+    res.end("fetching user by id");
+})
+app.post("/users", (req,res)=> {
+    res.end("adding user");
+})
+app.patch("/users/:id", (req,res)=> {
+    res.end("editing a particular user");
+})
+app.delete("/users/:id", (req, res)=>{
+    res.end("deleting a particular user");
+})
+
 
 
 
